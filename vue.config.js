6abcc51c6b3ac9f7e889f8f9825f387761e6a920
Marcus Amaral Rolim <m4rkux@gmail.com>
2021-08-27 18:24:38 -0300

    Fixing cache issue

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,8 @@ module.exports = {
   devServer: {
     host: "localhost",
   },
+  configureWebpack: (config) => {
+    config.output.filename = "js/[name].[contenthash:8].min.js";
+    config.output.chunkFilename = "js/[name].[contenthash:8].min.js";
+  },
 };
